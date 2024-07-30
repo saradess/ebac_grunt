5431dcfff16361ab5959d89ca905a1610fa8c31c
Sara Oliveira <sara.desider@gmail.com>
2024-07-30 11:50:26 -0300

    correção na gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.js'
+                            replacement: '../src/scripts/main.min.js'
                         }
                     ]
                 },
